3ebf5af335169d5f097a50aa0ef9e5348c20d46a
Triet Le <triet.le@plus3it.com>
2019-08-28 12:33:32 -0400

    Updates module test for both py2 and py3

---
+++ tests/vault-py2/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+resource "random_id" "name" {
+  byte_length = 6
+  prefix      = "tf-vault-"
+}
+
+module "vault-py2" {
+  source = "../../"
+
+  environment      = var.environment
+  desired_capacity = 1
+  ami_owner        = var.ami_owner
+
+  name           = "${random_id.name.hex}-py2"
+  key_pair_name  = var.key_pair_name
+  kms_key_id     = var.kms_key_id
+  ec2_subnet_ids = var.ec2_subnet_ids
+  lb_subnet_ids  = var.lb_subnet_ids
+
+  cloudwatch_agent_url = var.cloudwatch_agent_url
+
+  domain_name     = var.domain_name
+  route53_zone_id = var.route53_zone_id
+
+  # Vault settings
+  vault_version  = var.vault_version
+  dynamodb_table = var.dynamodb_table
+
+  # Watchmaker settings
+  watchmaker_config = var.watchmaker_config
+
+  toggle_update = "B"
+}
--- tests/vault-py2/variables.tf
+++ tests/vault-py2/variables.tf
@@ -1,59 +1,65 @@
 variable "environment" {
   description = "Type of environment -- must be one of: dev, test, prod"
-  type        = "string"
+  type        = string
   default     = "test"
 }
+
 variable "key_pair_name" {
   description = "Keypair to associate to launched instances"
-  type        = "string"
-  default     = ""
+  type        = string
 }
 
 variable "ami_owner" {
   description = "Account id/alias of the AMI owner"
-  type        = "string"
+  type        = string
 }
 
 variable "ec2_subnet_ids" {
   description = "List of subnets where EC2 instances will be launched"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "lb_subnet_ids" {
   description = "List of subnets to associate to the Load Balancer"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "vault_version" {
   description = "Version of Vault to be installed on servers"
-  type        = "string"
+  type        = string
 }
 
 variable "kms_key_id" {
   description = "Id of an AWS KMS key use for auto unseal operation when vault is intialize"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "domain_name" {
-  type        = "string"
+  type        = string
   description = "Domain to provision test vault cluster"
+}
+
+variable "route53_zone_id" {
+  type        = string
+  description = "Hosted zone ID Route 53 hosted zone"
 }
 
 variable "dynamodb_table" {
   description = "Name of the Dynamodb to be used as storage backend for Vault"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "cloudwatch_agent_url" {
-  type        = "string"
+  type        = string
   description = "(Optional) S3 URL to CloudWatch Agent installer. Example: s3://amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip"
   default     = ""
 }
 
 variable "watchmaker_config" {
-  type        = "string"
+  type        = string
   description = "(Optional) URL to a Watchmaker config file"
   default     = ""
 }
+
