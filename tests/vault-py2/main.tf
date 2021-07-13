f0a133bf6ef179304ca2162a9c42340e2224f698
Karri Balk <kbalk@users.noreply.github.com>
2021-07-13 13:24:35 -0400

    Initial changes for automatic integration tests

--- tests/vault-py2/main.tf
+++ tests/vault-py2/main.tf
@@ -1,7 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
 data "terraform_remote_state" "prereq" {
   backend = "local"
   config = {
--- tests/vault-py3/main.tf
+++ tests/vault-py3/main.tf
@@ -1,7 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
 resource "random_id" "name" {
   byte_length = 6
   prefix      = "tf-vault-"
